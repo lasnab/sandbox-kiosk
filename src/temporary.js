c3304aa969fc1fa335faa118fdee29088ef3ab1d
lasnab <hrithikbansal70@gmail.com>
2021-03-04 01:12:16 +0530

    prepare for deploy

---
+++ src/temporary.js
@@ -0,0 +1,118 @@
+const tool_response = [
+    {
+      "studio_id": "1224",
+      "tool_id": "Belt & Disk Sander",
+      "description_url": "",
+      "image_url": "https://wiki.umiacs.umd.edu/sandbox/images/a/a2/Jet_jsg96.jpg",
+      "training_url": "",
+      "drive_url": ""
+    },
+    {
+      "studio_id": "1224",
+      "tool_id": "Cordless Drill",
+      "description_url": "",
+      "image_url": "https://wiki.umiacs.umd.edu/sandbox/images/2/25/Dewalt_dcd771.jpg",
+      "training_url": "",
+      "drive_url": ""
+    },
+    {
+      "studio_id": "1224",
+      "tool_id": "Cordless Jigsaw",
+      "description_url": "",
+      "image_url": "https://wiki.umiacs.umd.edu/sandbox/images/8/81/Jigsaw-DCS331.png",
+      "training_url": "",
+      "drive_url": ""
+    },
+    {
+      "studio_id": "1224",
+      "tool_id": "Impact Driver",
+      "description_url": "",
+      "image_url": "https://wiki.umiacs.umd.edu/sandbox/images/1/1b/Dewalt_dcf809.jpg",
+      "training_url": "",
+      "drive_url": ""
+    },
+    {
+      "studio_id": "1224",
+      "tool_id": "Jet JDP-17 Drill Press",
+      "description_url": "",
+      "image_url": "https://wiki.umiacs.umd.edu/sandbox/images/c/cd/Jet-JDP17-drillpress.png",
+      "training_url": "",
+      "drive_url": ""
+    },
+    {
+      "studio_id": "1224",
+      "tool_id": "Miter Saw",
+      "description_url": "",
+      "image_url": "https://wiki.umiacs.umd.edu/sandbox/images/f/fc/Festool_kapex_ks120.jpeg",
+      "training_url": "",
+      "drive_url": ""
+    },
+    {
+      "studio_id": "1224",
+      "tool_id": "Orbital Sander",
+      "description_url": "",
+      "image_url": "https://wiki.umiacs.umd.edu/sandbox/images/d/db/Craftsman_11218.jpg",
+      "training_url": "",
+      "drive_url": ""
+    },
+    {
+      "studio_id": "1224",
+      "tool_id": "Oscillating MultiTool",
+      "description_url": "",
+      "image_url": "https://wiki.umiacs.umd.edu/sandbox/images/6/6b/Dewalt_dcs354.jpg",
+      "training_url": "",
+      "drive_url": ""
+    },
+    {
+      "studio_id": "1224",
+      "tool_id": "Reciprocating Saw",
+      "description_url": "",
+      "image_url": "https://wiki.umiacs.umd.edu/sandbox/images/7/73/Dewalt_dcs387.jpg",
+      "training_url": "",
+      "drive_url": ""
+    },
+    {
+      "studio_id": "1224",
+      "tool_id": "Rotary Tool",
+      "description_url": "",
+      "image_url": "https://wiki.umiacs.umd.edu/sandbox/images/2/2c/Dremel_4000.jpg",
+      "training_url": "",
+      "drive_url": ""
+    },
+    {
+      "studio_id": "1224",
+      "tool_id": "Router",
+      "description_url": "",
+      "image_url": "https://wiki.umiacs.umd.edu/sandbox/images/7/72/Dewalt_dwp611.jpg",
+      "training_url": "",
+      "drive_url": ""
+    },
+    {
+      "studio_id": "1224",
+      "tool_id": "Router Table",
+      "description_url": "",
+      "image_url": "https://wiki.umiacs.umd.edu/sandbox/images/f/f4/Jessem_09422.jpg",
+      "training_url": "",
+      "drive_url": ""
+    },
+    {
+      "studio_id": "1224",
+      "tool_id": "Scroll Saw",
+      "description_url": "",
+      "image_url": "https://wiki.umiacs.umd.edu/sandbox/images/c/c9/S-l1600.jpg",
+      "training_url": "",
+      "drive_url": ""
+    },
+    {
+      "studio_id": "1224",
+      "tool_id": "Track Saw",
+      "description_url": "",
+      "image_url": "https://wiki.umiacs.umd.edu/sandbox/images/2/27/Festool_ts55.jpg",
+      "training_url": "",
+      "drive_url": ""
+    }
+  ]
+
+  module.exports = {
+    tool_response
+  }
