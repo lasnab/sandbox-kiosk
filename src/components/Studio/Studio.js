445097d6662e9ee64134a0a7d6e5cfcb16dd4af0
lasnab <hrithikbansal70@gmail.com>
2021-03-04 20:16:01 +0530

    fixed studio css

--- src/components/Studio/Studio.js
+++ src/components/Studio/Studio.js
@@ -4,16 +4,16 @@ import { Link } from 'react-router-dom'
 
 function Studio({ studio }) {
     return (
-        <div className='studio'>
-            <h1 className='studio__name'>{ studio } Studio</h1>
+        <div className='studio cover'>
+            <div className='studio__heading'>{ studio } Studio</div>
             <div className='studio__links'>
                 <Link to='info' className='studio-option'>Studio Information</Link>
                 <Link to='reserve' className='studio-option'>Reserve Studio</Link>
-                <Link to='' className='studio-option'>Checkout Tool</Link>
-                <Link to='' className='studio-option'>Return Tool</Link>
+                <Link to='/' className='studio-option'>Checkout Tool</Link>
+                <Link to='/' className='studio-option'>Return Tool</Link>
                 <Link to='alltools' className='studio-option'>All Tools</Link>
                 <Link to='tool' className='studio-option'>UID Information</Link>
-                <Link to='' className='studio-option'>Studio Experts</Link>
+                <Link to='/' className='studio-option'>Studio Experts</Link>
             </div>
         </div>
     )
